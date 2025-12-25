09567dd113d409d16ac26100f73cc0fc2396448f
Veltman Jr <161249791+VokeyJ7@users.noreply.github.com>
2025-12-25 16:54:37 -0500

    Refactor ResidualBlock and FeatureExtractionModule
    Updated BatchNorm2d initialization and added parameters to FeatureExtractionModule. Improved forward method to include dropout and sequential layers.

--- model.py
+++ model.py
@@ -2,14 +2,15 @@ import torch
 import torch.nn as nn
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_features, out_features, kernel_size, stride, momentum, padding):
+    def __init__(self, in_features, out_features, kernel_size, stride, momentum, padding): 
         super(ResidualBlock, self).__init__()
+        
         self.conv1 = nn.Conv2d(in_features, out_features, kernel_size, stride, padding)
         self.conv2 = nn.Conv2d(in_features, out_features, kernel_size, stride, padding) 
         self.conv3 = nn.Conv2d(out_features, out_features, kernel_size, stride, padding) 
-        self.bn1 = nn.BatchNorm2d(out_features, momentum)
-        self.bn2 = nn.BatchNorm2d(out_features, momentum)
-        self.bn3 = nn.BatchNorm2d(out_features, momentum)
+        self.bn1 = nn.BatchNorm2d(out_features, momentum=momentum)
+        self.bn2 = nn.BatchNorm2d(out_features, momentum=momentum)
+        self.bn3 = nn.BatchNorm2d(out_features, momentum=momentum)
         self.relu = nn.ReLU()
         
     def forward(self, x):
@@ -25,15 +26,48 @@ class ResidualBlock(nn.Module):
         return out
 
 class FeatureExtractionModule(nn.Module):
-    def __init__(self, in_features, out_features, kernel_size, pool_size, stride, momentum):
+    def __init__(self, in_features, out_features, kernel_size, pool_size, conv_stride, pool_stride, dropout, momentum, num_layers, softmax_dim, padding):
         super(FeatureExtractionModule, self).__init__()
-        self.conv1 = nn.Conv2d(in_features, out_features, stride)
-        self.bn1 = nn.BatchNorm2d(out_features, momentum)
-        self.relu = nn.relu()
-        self.mp = nn.MaxPool2d(pool_size, stride)
-        self.rb = ResidualBlock(in_features, out_features, kernel_size, stride)
-        self.conv2 = nn.Conv2d(out_features, out_features, kernel_size, stride)
-        ### flatten
+        self.conv1 = nn.Conv2d(in_features, out_features, kernel_size, stride=conv_stride, padding=padding)
+        self.bn1 = nn.BatchNorm2d(out_features, momentum=momentum)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(pool_size, pool_stride)
+        self.layers = nn.ModuleList([ResidualBlock(out_features, out_features, kernel_size, conv_stride, momentum, padding) for _ in range(num_layers)])
+        self.conv2 = nn.Conv2d(out_features, out_features, kernel_size, stride=conv_stride, padding=padding)
+        self.flatten = nn.Flatten()
+        self.dropout1 = nn.Dropout(dropout)
+        self.fc1 = nn.Sequential(
+            nn.Linear(#trouble finding dimensions after flattening, 64),
+            nn.ReLU()
+        )
+        self.dropout2 = nn.Dropout(dropout)
+        self.fc2 = nn.Sequential(
+            nn.Linear(64, 7),
+            nn.Softmax(dim=softmax_dim)
+        )
+        
+
+    def forward(self, x):
+        fwd = self.conv1(x)
+        fwd = self.bn1(fwd)
+        fwd = self.relu(fwd)
+        fwd = self.maxpool(fwd)
+        for layer in self.layers:
+            fwd = layer(fwd)
+    
+        fwd = self.conv2(fwd)
+        fwd = self.flatten(fwd)
+        fwd = self.dropout1(fwd)
+        feature_vectors = self.fc1(fwd)
+        fwd = self.dropout2(feature_vectors)
+        out = self.fc2(fwd)
+        return feature_vectors, out
+        
+        
+        
+        
+        
+        
         
         
         
