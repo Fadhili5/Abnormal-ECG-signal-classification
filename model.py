eccbe6c1a5523546c27c66299ce5ff0e45ed7b9e
Veltman Jr <161249791+VokeyJ7@users.noreply.github.com>
2026-01-03 16:22:36 -0600

    Implement ECG data loading and preprocessing
    Added data loading and preprocessing for ECG signals, including Z-score normalization and RPM tensor generation. Mapped heart conditions to indices for classification.

--- model.py
+++ model.py
@@ -1,6 +1,97 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
+import pandas as pd
+import os
+import numpy as np
+from torch.utils.data import TensorDataset, DataLoader
+
+arr = []
+ids = []
+root = "." 
+target_dir = "ECGDataDenoised"
+for dirpath, dirnames, filenames in os.walk(root):
+    if target_dir in dirnames:
+        target_path = os.path.join(dirpath, target_dir)
+
+        for file in os.listdir(target_path):
+            if file.lower().endswith(".csv"):
+                file_path = os.path.join(target_path, file)
+                df = pd.read_csv(file_path)
+                lead_ii = df.iloc[:, 1].to_numpy()
+                arr.append(lead_ii)
+                ids.append(os.path.splitext(file)[0])  
+        break
+
+pairs = [(rid, torch.from_numpy(sig).float()) for rid, sig in zip(ids, arr)]
+pairs = [(rid, x) for rid, x in pairs if len(x) == 4999]
+
+ids_filtered = [rid for rid, _ in pairs]
+x_list_filtered = [x for _, x in pairs]
+
+# RPM Workflow
+def Z_score(input_tensor, eps=1e-8):
+    out = []
+    for x in input_tensor:
+        x = (x - x.mean()) / (x.std(unbiased=False) + eps)
+        out.append(x)
+    return out
+
+
+x_list_norm = Z_score(x_list_filtered)  
+m = 72
+y_list = [F.adaptive_avg_pool1d(x.view(1,1,-1), m).squeeze(0).squeeze(0) for x in x_list_norm]
+
+y = y_list[0]
+
+rpm_tensors = []
+
+for y in y_list:
+    rpm = y[:, None] - y[None, :]
+    rpm_min = rpm.min()
+    rpm_max = rpm.max()
+
+    rpm_img = (rpm - rpm_min) / (rpm_max - rpm_min + 1e-8)
+    rpm_tensors.append(rpm_img.unsqueeze(0).float())
+    
+
+x_rpm = torch.stack(rpm_tensors, dim=0)
+print(x_rpm.shape)
+
+# mapping the heart conditions to indices
+diag = pd.read_excel(os.path.join(root, "Diagnostics.xlsx"))
+id_to_labelname = dict(zip(diag.iloc[:,0].astype(str), diag.iloc[:, 1].astype(str)))
+
+class_map = {
+    "SB": 0,
+    "SR": 1,
+    "AFIB": 2,
+    "ST": 3,
+    "AF": 4,
+    "SI": 5,
+    "SVT": 6,
+    "AT": 7,
+    "AVNRT": 8,
+    "AVRT": 9,
+    "SAAWR": 10
+}
+
+
+y_list = []
+keep_indices = []
+
+for i, rid in enumerate(ids_filtered):
+    if rid in id_to_labelname:
+        label_name = id_to_labelname[rid].upper().strip()
+
+        if label_name in class_map:
+            y_list.append(class_map[label_name]) # maps classes to indices then appends indices as labels
+            keep_indices.append(i)
+
+
+x = x_rpm[keep_indices]
+y = torch.tensor(y_list, dtype=torch.long)
+
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_features, out_features, kernel_size, stride, momentum, padding): 
@@ -118,8 +209,13 @@ class ClassificationModule(nn.Module):
 
         return x
             
-        
-        
+
+dataset = TensorDataset(x, y)
+
+len_ds = len(dataset)
+train = int(0.8 * len_ds)
+val = len_ds - train
+
         
         
         
